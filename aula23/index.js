96482eb1882b634a8bc6a656e6d9e0edc658ea8e
Vroot-del <victorhvoa@gmail.com>
2022-04-18 08:58:05 -0300

    Curto-circuito

---
+++ aula23/index.js
@@ -0,0 +1,14 @@
+//function falaOi(){
+//    return 'Oi';
+//}
+
+//const vaiExecutar = 'Joazinho';
+
+//console.log(vaiExecutar && falaOi());
+
+//console.log(0 || false || null || 'Victor Oliveira' || true);
+
+const corUsuario = 'vermelho';
+const corPadrao = corUsuario || 'preto'; 
+
+console.log(corPadrao);
